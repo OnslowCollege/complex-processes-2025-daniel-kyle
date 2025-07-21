97c81f6a5f256041b7b3b271a00725e8ff4fea15
daniel <daniel.mostert@student.onslow.school.nz>
2025-07-22 10:16:45 +1200

    practise gleam

--- build/dev/javascript/file_terminal/file_terminal.mjs
+++ build/dev/javascript/file_terminal/file_terminal.mjs
@@ -1,73 +1,11 @@
 import * as $io from "../gleam_stdlib/gleam/io.mjs";
-import * as $result from "../gleam_stdlib/gleam/result.mjs";
-import { Ok } from "./gleam.mjs";
+import * as $string from "../gleam_stdlib/gleam/string.mjs";
 
-function get_username() {
-  return new Ok("kyle");
-}
-
-function get_password() {
-  return new Ok("hunter2");
-}
-
-function log_in(_, _1) {
-  return new Ok("Fuck off");
-}
-
-export function without_use() {
-  return $result.try$(
-    get_username(),
-    (username) => {
-      return $result.try$(
-        get_password(),
-        (password) => {
-          return $result.map(
-            log_in(username, password),
-            (greeting) => { return ((greeting + ", ") + username) + "\n"; },
-          );
-        },
-      );
-    },
-  );
-}
-
-export function with_use() {
-  return $result.try$(
-    get_username(),
-    (username) => {
-      return $result.try$(
-        get_password(),
-        (password) => {
-          return $result.map(
-            log_in(username, password),
-            (greeting) => { return ((greeting + ", ") + username) + "\n"; },
-          );
-        },
-      );
-    },
-  );
+export function is_capital_letter(char) {
+  let uppercase_char = $string.uppercase(char);
+  return $string.contains(uppercase_char, char);
 }
 
 export function main() {
-  let _block;
-  let $1 = without_use();
-  if ($1 instanceof Ok) {
-    let msg = $1[0];
-    _block = $io.print(msg);
-  } else {
-    _block = $io.print("Login failed\n");
-  }
-  let $ = _block;
-  
-  let _block$1;
-  let $3 = with_use();
-  if ($3 instanceof Ok) {
-    let msg = $3[0];
-    _block$1 = $io.print(msg);
-  } else {
-    _block$1 = $io.print("Login failed\n");
-  }
-  let $2 = _block$1;
-  
-  return $2;
+  return $io.println("A");
 }
--- src/file_terminal.gleam
+++ src/file_terminal.gleam
@@ -1,48 +1,10 @@
 import gleam/io
-import gleam/result
+import gleam/string
+pub fn is_capital_letter(char: String) -> Bool {
+    let uppercase_char = string.uppercase(char)
+    string.contains(uppercase_char, char)
+}
 
 pub fn main() {
-  let _ = case without_use() {
-    Ok(msg) -> io.print(msg)
-    Error(_) -> io.print("Login failed\n")
-  }
-
-  let _ = case with_use() {
-    Ok(msg) -> io.print(msg)
-    Error(_) -> io.print("Login failed\n")
-  }
-}
-
-pub fn without_use() -> Result(String, Nil) {
-  result.try(get_username(), fn(username) {
-    result.try(get_password(), fn(password) {
-      result.map(log_in(username, password), fn(greeting) {
-        greeting <> ", " <> username <> "\n"
-      })
-    })
-  })
-}
-
-pub fn with_use() -> Result(String, Nil) {
-  result.try(get_username(), fn(username) {
-    result.try(get_password(), fn(password) {
-      result.map(log_in(username, password), fn(greeting) {
-        greeting <> ", " <> username <> "\n"
-      })
-    })
-  })
-}
-
-// Simulated login functions
-
-fn get_username() -> Result(String, Nil) {
-  Ok("kyle")
-}
-
-fn get_password() -> Result(String, Nil) {
-  Ok("hunter2")
-}
-
-fn log_in(_username: String, _password: String) -> Result(String, Nil) {
-  Ok("Fuck off")
+  io.println(is_capital_letter("A")) // Should print true  
 }
